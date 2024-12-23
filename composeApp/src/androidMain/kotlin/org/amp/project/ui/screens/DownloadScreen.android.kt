d20199d4437dd841e54663e03004081c7db14c65
antoniomp17 <antoniomoraleda17@gmail.com>
2024-12-23 18:55:33 +0100

    #1.0-61: Scroll in DownloadContent

--- composeApp/src/androidMain/kotlin/org/amp/project/ui/screens/DownloadScreen.android.kt
+++ composeApp/src/androidMain/kotlin/org/amp/project/ui/screens/DownloadScreen.android.kt
@@ -2,9 +2,15 @@ package org.amp.project.ui.screens
 
 import androidx.compose.runtime.Composable
 import org.amp.project.data.utils.isWeb
+import org.amp.project.presentation.ThemeUiState
 import org.amp.project.ui.components.download.DownloadContent
 
 @Composable
-actual fun DownloadScreen() {
-    DownloadContent(isWeb = isWeb())
+actual fun DownloadScreen(
+    themeUiState: ThemeUiState
+) {
+    DownloadContent(
+        isWeb = isWeb(),
+        themeUiState = themeUiState
+    )
 }
--- composeApp/src/commonMain/kotlin/org/amp/project/ui/screens/DownloadScreen.kt
+++ composeApp/src/commonMain/kotlin/org/amp/project/ui/screens/DownloadScreen.kt
@@ -1,6 +1,7 @@
 package org.amp.project.ui.screens
 
 import androidx.compose.runtime.Composable
+import org.amp.project.presentation.ThemeUiState
 
 @Composable
-expect fun DownloadScreen()
+expect fun DownloadScreen(themeUiState: ThemeUiState)
